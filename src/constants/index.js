2f7a978594ab1adf74d1590f65b24c40e1fc92d4
Gomati <gomatis96@gmail.com>
2023-05-24 10:46:24 +0530

    Updates

--- src/constants/index.js
+++
@@ -1,5 +0,0 @@
-const CONSTANTS = {
-    ROOT_ID : 0, // ID of the highest level employee like CEO
-};
-
-export default CONSTANTS;
--- src/utils/index.js
+++
@@ -1,5 +0,0 @@
-const containsValue = (objToSearch, searchText) => {
-    return Object.values(objToSearch).some(val => val.toLowerCase().includes(searchText));
-};
-
-export default containsValue;
